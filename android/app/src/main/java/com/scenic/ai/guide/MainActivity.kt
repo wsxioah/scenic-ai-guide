68e2d41108807b81295a82df7303f1688f2a3966
wsxiaoh <13748841+wsxiaoh_0@user.noreply.gitee.com>
2026-06-09 19:17:29 +0800

    Change Android package name to com.scenic.ai.guide

--- android/app/src/main/java/com/scenic/ai/guide/MainActivity.kt
+++ android/app/src/main/java/com/scenic/ai/guide/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.mobile
+package com.scenic.ai.guide
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/scenic/ai/guide/MainApplication.kt
+++ android/app/src/main/java/com/scenic/ai/guide/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.mobile
+package com.scenic.ai.guide
 
 import android.app.Application
 import android.content.res.Configuration
